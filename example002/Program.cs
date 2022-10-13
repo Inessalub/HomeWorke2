8e37d5657b6f0da48331e8ff6ee95f0b2292a7e3
unknown <inessalub@gmail.com>
2022-10-13 11:05:03 +0400

    example 003

---
+++ example002/Program.cs
@@ -0,0 +1,16 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+if(a == b*b) 
+{
+    Console.WriteLine("Число является квадратом другого");
+}
+else
+{
+    Console.WriteLine("Число не является квадратом другого");
+}
---
+++ example003/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите число N от 1 до 4: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+if (N == 1)
+{
+    Console.WriteLine("x>0; y>0");
+}
+if (N == 2)
+{
+    Console.WriteLine("x<0; y>0");
+}
+if (N == 3)
+{
+    Console.WriteLine("x<0; y<0");
+}
+if (N == 4)
+{
+    Console.WriteLine("x>0; y<0");
+}
